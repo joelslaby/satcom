30030607e2c88044755e0d6c1a9a4f8bd558a03e
The-SI-Guy <drewdisharoon@gmail.com>
2021-11-10 14:09:53 -0500

    Added Parts List and A "Noise" Model with no AGC

--- PrepareLink.m
+++ PrepareLink.m
@@ -1,5 +1,6 @@
 % Transmit
-
+BW = 5e9;
+k = 1.38064852e-23;
 Fs_tx = 1e6; % Hz
 Ts_tx = 1/Fs_tx; % s
 R = 35768; % km
@@ -19,7 +20,17 @@ G_ant_rx = 40; % dB
 Fs_rx = 1e6;
 Ts_rx = 1/Fs_rx;
 fc_rx = 73.5e9/1e6; % MHz
-
+N_ant_rx = k*273*BW; 
+N_mix_rx = k*300*BW;
+G_mix_rx = 10^(-8/10);
+G_hpa_rx = 10^(20/10);
+G_lna_rx = 10^(10/10);
+N_lna_rx = k*300*BW;
+G_bpf_rx = 10^(-0.6/10);
+G_lpf_rx = 10^(-1.6/10);
+N_bpf_rx = k*300*BW;
+N_lpf_rx = k*300*BW;
+N_hpa_rx = k*300*BW;
 v = 3336.8; %m/s
 
 lambda_tx = 3e8/(fc_tx*1e6);
