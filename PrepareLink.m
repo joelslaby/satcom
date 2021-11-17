ea798d37cb24b8991d2cc5b0cca47342e654f9f8
The-SI-Guy <drewdisharoon@gmail.com>
2021-11-17 14:10:49 -0500

    UpdatedModelForRealDoppler

--- PrepareLink.m
+++ PrepareLink.m
@@ -22,18 +22,17 @@ Kfactor = 10;
 G_ant_rx = 40; % dB
 Fs_rx = 1e6;
 Ts_rx = 1/Fs_rx;
-fc_rx = 73.5e9/1e6; % MHz
-N_ant_rx = k*273*BW; 
-N_mix_rx = k*300*BW;
+fc_rx = 73.5e9/1e6; % MHz 
 G_mix_rx = 10^(-8/10);
 G_hpa_rx = 10^(20/10);
 G_lna_rx = 10^(10/10);
-N_lna_rx = k*300*BW;
 G_bpf_rx = 10^(-0.6/10);
 G_lpf_rx = 10^(-1.6/10);
-N_bpf_rx = k*300*BW;
-N_lpf_rx = k*300*BW;
-N_hpa_rx = k*300*BW;
+G_bpf_rx2 = 10^(-0.6/10);
+G_hpa_rx2 = 10^(35/10);
+G_mix2_rx = 10^(-8/10);
+
+
 v = 3336.8; %m/s
 
 lambda_tx = 3e8/(fc_tx*1e6);
